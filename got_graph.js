0851ea642e4e7fa90ea5575c3fbe2e73820cbbc3
Erik <erik.svedang@gmail.com>
2013-12-10 20:12:49 +0100

    remove transform.js

--- got_graph.js
+++ got_graph.js
@@ -1,3 +1,4 @@
 goog.addDependency("base.js", ['goog'], []);
 goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
+goog.addDependency("../got_graph/transform.js", ['got_graph.transform'], ['cljs.core']);
 goog.addDependency("../got_graph/core.js", ['got_graph.core'], ['cljs.core']);
--- got_transform.js
+++
@@ -1,5 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
-goog.addDependency("../clojure/walk.js", ['clojure.walk'], ['cljs.core']);
-goog.addDependency("../got_graph/transform.js", ['got_graph.transform'], ['cljs.core', 'clojure.walk']);
-goog.addDependency("../got_graph/core.js", ['got_graph.core'], ['cljs.core']);
