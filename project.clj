f5304ecd25b8c1acdd06b479778eb607b813ff21
Erik <erik.svedang@gmail.com>
2013-12-10 18:42:14 +0100

    cljsbuild setup

--- project.clj
+++ project.clj
@@ -3,4 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2030"]]
+
+  :plugins [[lein-cljsbuild "1.0.0-alpha2"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {
+    :builds [{:id "got-graph"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "got-graph.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}]})
