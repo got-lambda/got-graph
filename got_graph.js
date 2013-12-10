c98a162c4cd8d1af668c56284401be6a212abe32
Erik <erik.svedang@gmail.com>
2013-12-10 20:34:47 +0100

    removed output

--- got_graph.js
+++
@@ -1,4 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
-goog.addDependency("../got_graph/transform.js", ['got_graph.transform'], ['cljs.core']);
-goog.addDependency("../got_graph/core.js", ['got_graph.core'], ['cljs.core']);
--- out/got_graph/core.cljs
+++
@@ -1,6 +0,0 @@
-(ns got-graph.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
